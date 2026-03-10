652432e5b069538a43342f0ea410b564ac6be468
gracebbakery <baker827@purdue.edu>
2026-03-10 16:37:10 -0400

    first commit

---
+++ src/app/nav.js
@@ -0,0 +1,12 @@
+const nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default nav;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import nav from "../app/nav"
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <nav />
       <main className={styles.main}>
         <Image
           className={styles.logo}
